57471c8709373215ce38a9b21794d752f79cbab8
sam-fritz1 <fritz1@ualberta.ca>
2024-10-25 11:32:15 -0600

    Exercise - with getCount method - expecting pass

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -51,7 +51,7 @@ public class CustomList extends ArrayAdapter<City> {
      * @return
      */
     public int getCount(){
-        return 0;
+        return cities.size();
     }
     /**
      * this adds a city object to the list
